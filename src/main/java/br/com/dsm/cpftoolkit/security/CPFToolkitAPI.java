bcb10473e0096675cd9933261ff11802381677f4
douglas-melo <12987598+douglas-melo@users.noreply.github.com>
2020-04-23 16:13:10 -0300

    It was included new methods, the previous methods aren't useful anymore.

---
+++ src/main/java/br/com/dsm/cpftoolkit/security/CPFToolkitAPI.java
@@ -0,0 +1,41 @@
+package br.com.dsm.cpftoolkit.security;
+
+/*
+ * Esta API fornece os métodos utilitários para a validação do CPF.
+ */
+final class CPFToolkitAPI {
+
+    private CPFToolkitAPI() {
+        throw new AssertionError("This class must not be instantiated.");
+    }
+
+    private static StatusProvider statusProvider(String cpf) {
+        return StatusProvider
+                .configureCPF(cpf)
+                .putUnstrustedCPF()
+                .putUnstrustedCPFValidator()
+                .putReasonProvider()
+                .putMessageBus()
+                .putMessageReporter();
+    }
+
+    public static boolean isCPFTrusted(String cpf) {
+        return statusProvider(cpf).provideStatus();
+    }
+
+    public static Reason getReason(String cpf) {
+        return statusProvider(cpf).provideReason();
+    }
+
+    public static String getReasonToJSON(String cpf) {
+        return statusProvider(cpf).provideReason().toString().toLowerCase().replaceAll("[_]", "");
+    }
+
+    public static int getCode(String cpf) {
+        return statusProvider(cpf).provideCode();
+    }
+
+    public static String getMessage(String cpf) {
+        return statusProvider(cpf).provideMessage();
+    }
+}
