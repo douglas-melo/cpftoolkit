14474abee4f22acb79f364975eee4e33fb184d9c
douglas-melo <12987598+douglas-melo@users.noreply.github.com>
2020-04-23 16:39:58 -0300

    This class was changed to provide an object that represents successful CPF check digit validation.

---
+++ src/main/java/br/com/dsm/cpftoolkit/security/AcceptedCheckDigit.java
@@ -0,0 +1,16 @@
+package br.com.dsm.cpftoolkit.security;
+
+/*
+ * Esta classe fornece o motivo válido caso a validação do dígito verificador
+ * seja positiva.
+ *
+ * Não implementou a interface diretamente porque o contrato de validação
+ * dos dígitos não é necessário nesta classe.
+ */
+final class AcceptedCheckDigit extends CheckDigitValidator {
+
+    @Override
+    public Reason getReason() {
+        return Reason.TRUSTED_CPF;
+    }
+}
