46d463f3db74b07aff7e13bf04b310ee9720abf8
douglas-melo <12987598+douglas-melo@users.noreply.github.com>
2020-04-23 18:11:47 -0300

    Adding new class to store the validation description messages.

---
+++ src/main/java/br/com/dsm/cpftoolkitapi/security/MessageBus.java
@@ -0,0 +1,34 @@
+package br.com.dsm.cpftoolkitapi.security;
+
+/*
+ * Esta classe contém os métodos para recuperar a mensagem de
+ * descrição da validação.
+ */
+final class MessageBus {
+
+    private UntrustedCPF cpf;
+
+    MessageBus(UntrustedCPF cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSuccessMessage() {
+        return "O CPF informado é válido.";
+    }
+
+    public String getNullMessage() {
+        return "O CPF informado é nulo.";
+    }
+
+    public String getEmptyMessage() {
+        return "O CPF informado é vazio.";
+    }
+
+    public String getPatternMessage() {
+        return "O CPF " + cpf.getCPF() + " não possui um padrão de dígitos válido.";
+    }
+
+    public String getCheckDigitsMessage() {
+        return "O CPF " + cpf.getCPF() + " é inválido. Não foi possível calcular o dígito verificador.";
+    }
+}
