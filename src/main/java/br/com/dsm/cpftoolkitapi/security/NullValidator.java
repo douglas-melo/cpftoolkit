22cea5d4a0d5799de99d77975d12cf93869de3ed
douglas-melo <12987598+douglas-melo@users.noreply.github.com>
2020-04-23 18:06:34 -0300

    Adding new class for null digits treatment.

---
+++ src/main/java/br/com/dsm/cpftoolkitapi/security/NullValidator.java
@@ -0,0 +1,32 @@
+package br.com.dsm.cpftoolkitapi.security;
+
+import java.util.Optional;
+
+/*
+ * Classe para validar os dígitos null do CPF inconsistente.
+ *
+ * Possui os métodos para validar os dígitos null a fim
+ * de obter um CPF consistente.
+ */
+final class NullValidator implements EvaluableCPF {
+
+    private UntrustedCPF cpf;
+
+    NullValidator(UntrustedCPF cpf) {
+        this.cpf = cpf;
+    }
+
+    private Optional<String> hasNullDigits() {
+        return Optional.ofNullable(cpf.getCPF());
+    }
+
+    @Override
+    public int validateDigits() {
+        return hasNullDigits().isPresent() ? 0 : 1;
+    }
+
+    @Override
+    public Reason getReason() {
+        return Reason.NULL_CPF;
+    }
+}
