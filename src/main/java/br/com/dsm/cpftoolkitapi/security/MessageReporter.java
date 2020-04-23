6c51039307bdbc2820d109935085400e0cafb783
douglas-melo <12987598+douglas-melo@users.noreply.github.com>
2020-04-23 18:22:20 -0300

    Adding new class to report description message by reason as criteria.

---
+++ src/main/java/br/com/dsm/cpftoolkitapi/security/MessageReporter.java
@@ -0,0 +1,38 @@
+package br.com.dsm.cpftoolkitapi.security;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * Esta classe mapeia a mensagem de descrição ao motivo da validação.
+ * A captura da mensagem de descrição será feita a partir do critério do motivo da validação.
+ */
+final class MessageReporter {
+
+    private ReasonProvider reasonProvider;
+    private MessageBus messageBus;
+    private String message;
+
+    MessageReporter(ReasonProvider reasonProvider, MessageBus messageBus) {
+        this.reasonProvider = reasonProvider;
+        this.messageBus = messageBus;
+
+        loadMessageCatalog();
+    }
+
+    private void loadMessageCatalog() {
+        Map<Reason, String> messageCatalog = new HashMap<>();
+
+        messageCatalog.put(Reason.NULL_CPF, messageBus.getNullMessage());
+        messageCatalog.put(Reason.EMPTY_CPF, messageBus.getEmptyMessage());
+        messageCatalog.put(Reason.INVALID_PATTERN, messageBus.getPatternMessage());
+        messageCatalog.put(Reason.INVALID_CPF, messageBus.getCheckDigitsMessage());
+        messageCatalog.put(Reason.TRUSTED_CPF, messageBus.getSuccessMessage());
+
+        message = messageCatalog.get(reasonProvider.getReason());
+    }
+
+    public String reportMessage() {
+        return message;
+    }
+}
