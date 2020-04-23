a35b4048d8ccbd5cff38c35409443b1cd4694ce4
douglas-melo <12987598+douglas-melo@users.noreply.github.com>
2020-04-23 16:17:27 -0300

    This Enum only handling with validation reasons now, it doesn't provide any description message anymore.

---
+++ src/main/java/br/com/dsm/cpftoolkit/security/Reason.java
@@ -0,0 +1,30 @@
+package br.com.dsm.cpftoolkit.security;
+
+/*
+ * Este enum implementa e contém os métodos para recuperar a causa,
+ * que será configurada de acordo com o resultado da validação.
+ */
+enum Reason {
+
+    TRUSTED_CPF(0, true),
+    NULL_CPF(1, false),
+    EMPTY_CPF(2, false),
+    INVALID_PATTERN(3, false),
+    INVALID_CPF(4, false);
+
+    private int code;
+    private boolean status;
+
+    Reason(int code, boolean status) {
+        this.code = code;
+        this.status = status;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public boolean hasTrustedStatus() {
+        return status;
+    }
+}
