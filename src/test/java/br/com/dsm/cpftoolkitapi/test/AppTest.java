425991ddf054a54c8c06d67f1e48ae47ec611f48
douglas-melo <12987598+douglas-melo@users.noreply.github.com>
2020-04-27 13:29:06 -0300

    This file won't be used anymore, cause two others files were included for test purposes.

--- src/test/java/br/com/dsm/cpftoolkitapi/test/AppTest.java
+++
@@ -1,9 +0,0 @@
-package br.com.dsm.cpftoolkitapi.test;
-
-//import org.junit.Assert;
-//import org.junit.Test;
-//import static org.junit.Assert.assertEquals;
-
-public class AppTest {
-    // TODO: The test cases needs to be applied.
-}
