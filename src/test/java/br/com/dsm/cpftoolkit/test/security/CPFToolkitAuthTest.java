2b970dedc64cc640bf6a552a765512807f9bcfe8
douglas-melo <12987598+douglas-melo@users.noreply.github.com>
2020-04-28 11:47:27 -0300

    TODO: Tests suite needs be applied.

---
+++ src/test/java/br/com/dsm/cpftoolkit/test/security/CPFToolkitAuthTest.java
@@ -0,0 +1,9 @@
+package br.com.dsm.cpftoolkit.test.security;
+
+//import org.junit.Assert;
+//import org.junit.Test;
+//import static org.junit.Assert.assertEquals;
+
+public class CPFToolkitAuthTest {
+    // TODO: The test cases needs to be applied.
+}
---
+++ src/test/java/br/com/dsm/cpftoolkit/test/util/CPFToolkitFormatterTest.java
@@ -0,0 +1,9 @@
+package br.com.dsm.cpftoolkit.test.util;
+
+//import org.junit.Assert;
+//import org.junit.Test;
+//import static org.junit.Assert.assertEquals;
+
+public class CPFToolkitFormatterTest {
+    // TODO: The test cases needs to be applied.
+}
