d68549fcb5e6f883dfabdf541cff7b7153c291cc
douglas-melo <12987598+douglas-melo@users.noreply.github.com>
2020-05-04 02:24:31 -0300

    Adding Javadoc.

--- src/main/java/br/com/dsm/cpftoolkit/security/CPFToolkitAuth.java
+++ src/main/java/br/com/dsm/cpftoolkit/security/CPFToolkitAuth.java
@@ -1,12 +1,18 @@
 package br.com.dsm.cpftoolkit.security;
 
-/*
+/**
  * Esta classe fornece os métodos para a validação do CPF.
+ *
+ * @author Douglas Melo
  */
 public final class CPFToolkitAuth {
 
     private StatusProvider statusProvider;
 
+    /**
+     * Constrói o objeto e inicializa o atributo.
+     * @param cpf O CPF, necessário para inicializar o atributo.
+     */
     public CPFToolkitAuth(String cpf) {
         statusProvider = new StatusReporter(cpf)
                 .putUnstrustedCPF()
@@ -17,23 +23,67 @@ public final class CPFToolkitAuth {
                 .report();
     }
 
+    /**
+     * Retorna o status booleano da validação do CPF.
+     * @return true caso o CPF seja válido.
+     */
     public boolean isCPFValid() {
         return statusProvider.provideStatus();
     }
 
+    /**
+     * Retorna o motivo indicando o status da validação.
+     * <br><br>
+     *
+     * TRUSTED_CPF <br>
+     * NULL_CPF <br>
+     * EMPTY_CPF <br>
+     * INVALID_PATTERN <br>
+     * INVALID_CPF <br>
+     *
+     * @return Uma String como o motivo indicando o status da validação.
+     */
     public String getReason() {
         return statusProvider.provideReason();
     }
 
+    /**
+     * Retorna o motivo indicando o status da validação para o formato JSON.
+     * <br><br>
+     *
+     * trustedcpf <br>
+     * nullcpf <br>
+     * emptycpf <br>
+     * invalidpattern <br>
+     * invalidcpf <br>
+     *
+     * @return Uma String como o motivo indicando o status da validação para o formato JSON.
+     */
     public String getReasonToJSON() {
         return statusProvider.provideReason().toLowerCase().replaceAll("[_]", "");
     }
 
+    /**
+     * Retorna um inteiro indicando o status da validação.
+     * <br><br>
+     *
+     * 0 - TRUSTED_CPF <br>
+     * 1 - NULL_CPF <br>
+     * 2 - EMPTY_CPF <br>
+     * 3 - INVALID_PATTERN <br>
+     * 4 - INVALID_CPF <br>
+     *
+     * @return Um inteiro indicando o status da validação.
+     */
     public int getCode() {
         return statusProvider.provideCode();
     }
 
-    public String getMessage() {
+    /**
+     * Retorna uma mensagem com a descrição do status da validação.
+     * @return Uma String como mensagem de descrição do status da validação.
+     */
+    public String getMessqage() {
         return statusProvider.provideMessage();
     }
 }
