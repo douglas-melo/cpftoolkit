e9e8484721658c27fec8b17bd7d6ec521968f17b
douglas-melo <12987598+douglas-melo@users.noreply.github.com>
2020-05-05 17:15:16 -0300

    Turning this class into Default access, and including some comments.

--- src/main/java/br/com/dsm/cpftoolkit/util/IllegalCPFArgumentException.java
+++ src/main/java/br/com/dsm/cpftoolkit/util/IllegalCPFArgumentException.java
@@ -4,11 +4,11 @@ package br.com.dsm.cpftoolkit.util;
  * Esta exceção será lançada nos casos em que um CPF inconsistente for
  * passado, e.g um CPF nulo, ou vazio, que dentro do contexto da aplicação são inválidos.
  */
-public class IllegalCPFArgumentException extends IllegalArgumentException {
+class IllegalCPFArgumentException extends IllegalArgumentException {
 
     private static final long serialVersionUID = 4479187854237180518L;
 
-    public IllegalCPFArgumentException(String message) {
+    IllegalCPFArgumentException(String message) {
         super(message);
     }
 }
