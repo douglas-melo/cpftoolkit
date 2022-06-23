5a133f5cd753ed54d2418c08a378bb7f9ab72c6f
douglas-melo <12987598+douglas-melo@users.noreply.github.com>
2022-06-23 17:23:10 -0300

    Removing this attribute, because it's became unnecessary.

--- src/main/java/br/com/dsm/cpftoolkit/security/UntrustedCPFValidator.java
+++ src/main/java/br/com/dsm/cpftoolkit/security/UntrustedCPFValidator.java
@@ -12,7 +12,6 @@ import java.util.List;
 final class UntrustedCPFValidator {
 
     private final UntrustedCPF cpf;
-    private List<EvaluableCPF> evaluableDigits;
     private Reason reason;
 
     UntrustedCPFValidator(UntrustedCPF cpf) {
