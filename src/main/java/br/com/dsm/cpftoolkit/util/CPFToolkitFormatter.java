ae4c7f5b775694b46c1872207d52466ed083c6dd
douglas-melo <12987598+douglas-melo@users.noreply.github.com>
2022-07-12 22:16:50 -0300

    Updating API following current context of application.

--- src/main/java/br/com/dsm/cpftoolkit/util/CPFToolkitFormatter.java
+++ src/main/java/br/com/dsm/cpftoolkit/util/CPFToolkitFormatter.java
@@ -16,13 +16,13 @@ public final class CPFToolkitFormatter {
     }
 
     /**
-     * Retorna uma instância estática da classe {@link PatternValidator}.
+     * Retorna uma instância estática da classe {@link PatternChecker}.
      *
      * @param cpf O CPF.
-     * @return Uma instância estática da classe {@link PatternValidator}.
+     * @return Uma instância estática da classe {@link PatternChecker}.
      */
-    private static PatternValidator patternValidator(String cpf) {
-        return PatternValidator.configureCPF(cpf);
+    private static PatternChecker patternChecker(String cpf) {
+        return PatternChecker.getInstance(cpf);
     }
 
     /**
@@ -32,7 +32,7 @@ public final class CPFToolkitFormatter {
      * @return O CPF com o formato XXXXXXXXXXX.
      */
     public static String removeDelimiter(String cpf) {
-        return patternValidator(cpf).getCPF();
+        return patternChecker(cpf).checkCPFPattern();
     }
 
     /**
@@ -42,7 +42,7 @@ public final class CPFToolkitFormatter {
      * @return O CPF com o formato XXX.XXX.XXX-XX.
      */
     public static String insertDelimiter(String cpf) {
-        return patternValidator(cpf).getCPF().replaceAll("(^.{3})(.{3})(.{3})", "$1.$2.$3-");
+        return patternChecker(cpf).checkCPFPattern().replaceAll("(^.{3})(.{3})(.{3})", "$1.$2.$3-");
     }
 
     /**
@@ -52,7 +52,7 @@ public final class CPFToolkitFormatter {
      * @return O dígito verificador do CPF.
      */
     public static String getCheckDigit(String cpf) {
-        return patternValidator(cpf).getCPF().substring(9, 11);
+        return patternChecker(cpf).checkCPFPattern().substring(9, 11);
     }
 
     /**
@@ -62,7 +62,7 @@ public final class CPFToolkitFormatter {
      * @return Os três primeiros dígitos do CPF.
      */
     public static String getFirstThreeDigits(String cpf) {
-        return patternValidator(cpf).getCPF().substring(0, 3);
+        return patternChecker(cpf).checkCPFPattern().substring(0, 3);
     }
 
     /**
@@ -72,7 +72,7 @@ public final class CPFToolkitFormatter {
      * @return Os três dígitos intermediários do CPF.
      */
     public static String getMiddleThreeDigits(String cpf) {
-        return patternValidator(cpf).getCPF().substring(3, 6);
+        return patternChecker(cpf).checkCPFPattern().substring(3, 6);
     }
 
     /**
@@ -82,6 +82,6 @@ public final class CPFToolkitFormatter {
      * @return Os três últimos dígitos do CPF.
      */
     public static String getLastThreeDigits(String cpf) {
-        return patternValidator(cpf).getCPF().substring(6, 9);
+        return patternChecker(cpf).checkCPFPattern().substring(6, 9);
     }
 }
